26adbedd48db059dcab4246002eb0b184b0adb0c
Parker Rebensdorf <parker.rebensdorf@gmail.com>
2018-03-16 16:35:27 -0700

    intro demo completed

--- src/index.js
+++ src/index.js
@@ -3,7 +3,21 @@ import ReactDOM from 'react-dom';
 
 import App from './components/app';
 
+
+
+
+const user = {
+    firstName: 'Parker',
+    lastName: 'Rebensdorf'
+}
+
+function greetUser(user){
+    return user.firstName + ' ' + user.lastName
+}
+
+const greeting = <h1>Hello, {greetUser(user)}</h1>
+
 ReactDOM.render(
-    <App />,
+    greeting,
     document.getElementById('root')
 );
