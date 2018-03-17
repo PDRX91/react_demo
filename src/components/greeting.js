126eee25e1637534e6eeaba70772f6ef9b0b22f7
Parker Rebensdorf <parker.rebensdorf@gmail.com>
2018-03-16 18:05:58 -0700

    functional component demo completed

---
+++ src/components/greeting.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Greeting(props){
+    console.log(props);
+
+    const {name, favColor} = props;
+
+    const greetingStyle = {
+        color: favColor
+    }
+
+    return <h1 style={greetingStyle}>Hello, {props.name}, welcome to our site!</h1>
+}
+
+export default Greeting;
